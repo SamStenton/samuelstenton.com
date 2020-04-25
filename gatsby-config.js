4996b52efd9f00f7be03fe690574769505b43322
Samuel Stenton <sthstenton@gmail.com>
2020-04-25 20:45:20 +0100

    add GA tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ module.exports = {
   siteMetadata: {
     title: `blog`,
     author: `Samuel Stenton`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    description: `A blog describing the learnings of a Cloud Engineer working with cool technologies.`,
+    siteUrl: `https://samuelstenton.com/`,
     social: {
       twitter: `samuel_stenton`,
     },
@@ -50,7 +50,23 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-57428259-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
       },
     },
     `gatsby-plugin-feed`,
